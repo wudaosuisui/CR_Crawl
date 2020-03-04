4aa2265218b44df69badaa9b4bfd2833e078e931
wudaosuisui <kuanglangqu@qq.com>
2020-03-04 22:19:55 +0800

    上传新项目

---
+++ CRcrawl/src/main/java/com/asiainfo/cr/crawl/Crawl.java
@@ -0,0 +1,11 @@
+package com.asiainfo.cr.crawl;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Crawl {
+    public static void main( String[] args ) {
+        System.out.println("Heool my crawl!");
+        System.out.println("There is nothing~ HA HA! ");
+    }
+}
